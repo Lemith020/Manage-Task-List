20669c2c2a14cdaa1d84b9ee3f07fe37b11ca61a
Lemith <lemithnanditha41@gmail.com>
2026-06-11 01:07:31 +0530

    change code

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,65 @@
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const cors = require('cors');
+// const connectDB = require('./config/db');
+// const authRoutes = require('./routes/authRoutes');
+// const taskRoutes = require('./routes/taskRoutes'); 
+
+// dotenv.config();
+// connectDB();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json()); 
+
+
+// app.use('/api/auth', authRoutes);
+// app.use('/api/tasks', taskRoutes); 
+
+// app.get('/', (req, res) => {
+//   res.send('Task Management System API is running...');
+// });
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running properly on port ${PORT}`);
+// });
+
+
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
-const taskRoutes = require('./routes/taskRoutes'); 
+const taskRoutes = require('./routes/taskRoutes');
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json()); 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  process.env.FRONTEND_URL
+];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes); 
+app.use('/api/tasks', taskRoutes);
 
 app.get('/', (req, res) => {
   res.send('Task Management System API is running...');
